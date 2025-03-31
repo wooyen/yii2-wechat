c02d677ae9b27523a4ec5e661439a540d384eefb
WU Yan <wuyan@jzsec.com>
2025-03-31 20:56:16 +0800

    serialize redis condition

--- src/utils/RedisCondition.php
+++ src/utils/RedisCondition.php
@@ -4,24 +4,38 @@ namespace yii\wechat\utils;
 
 use Yii;
 use yii\base\BaseObject;
+use yii\di\Instance;
+use yii\redis\Connection;
 class RedisCondition extends BaseObject implements Condition
 {
 	public $redis = 'redis';
+	private $_redis;
+	public function init()
+	{
+		parent::init();
+		$this->_redis = Instance::ensure($this->redis, Connection::class);
+	}
 	public function signal(string $key, $data)
 	{
-		$redis = Yii::$app->get($this->redis);
-		$redis->lpush($key, $data);
+		$this->_redis->lpush($key, $data);
 	}
 
 	public function wait(string $key)
 	{
-		$redis = Yii::$app->get($this->redis);
 		$timeout = ini_get('max_execution_time') * 0.8;
-		$result = $redis->brpop($key, $timeout);
+		$result = $this->_redis->brpop($key, $timeout);
 		if ($result) {
 			return $result[1];
 		}
 		return false;
+	}
+	public function __sleep()
+	{
+		return ['redis'];
+	}
+	public function __wakeup()
+	{
+		$this->init();
 	}
 }
 

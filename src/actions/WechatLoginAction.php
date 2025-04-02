4418b75f8e0124e81b60fb2dd1460d62e6b09b87
WU Yan <wuyan@jzsec.com>
2025-04-02 10:23:57 +0800

    merge oauth action and filter

--- src/actions/WechatLoginAction.php
+++ src/actions/WechatLoginAction.php
@@ -14,8 +14,6 @@ class WechatLoginAction extends Action
 {
 	public $wechat = 'wechat';
 	public $checkDelay = 1000;
-	public $oauthUrl;
-	public $backUrl;
 	public $qrTicketUrl;
 	public $statusUrl;
 	public $view = '@wechat/views/login';
@@ -28,18 +26,15 @@ class WechatLoginAction extends Action
 			$event->sender->controller->attachBehavior('wechatOauth', [
 				'class' => OAuthFilter::class,
 				'wechat' => $this->wechat,
-				'oauthUrl' => $this->oauthUrl,
-				'backUrl' => $this->backUrl,
 			]);
 		});
-		$this->wechat->on(Wechat::EVENT_OAUTH, [$this, 'onOAuth']);
 	}
 	public function run()
 	{
 		return $this->controller->render($this->view, [
 			'qrTicketUrl' => Url::toRoute($this->qrTicketUrl),
 			'statusUrl' => Url::toRoute($this->statusUrl),
-			'backUrl' => Url::toRoute($this->backUrl),
+			'backUrl' => Url::toRoute(Yii::$app->user->getReturnUrl()),
 			'checkDelay' => $this->checkDelay,
 		]);
 	}
